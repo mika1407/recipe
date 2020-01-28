5a8f9f9333a2a4b4fe7b8231bb7f5fef428fbd33
mika1407 <koneautomaatio@hotmail.com>
2020-01-28 23:45:10 +0200

    Updates

---
+++ precache-manifest.7fe099c3e95c087176cc130623cdbc40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7479788688cfb9e5b335c2519c5c2f2",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "60f16e501208926f11b9",
+    "url": "/recipe/static/css/main.e5e5ec7e.chunk.css"
+  },
+  {
+    "revision": "137720e5425a7cc913d8",
+    "url": "/recipe/static/js/2.72223848.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe/static/js/2.72223848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60f16e501208926f11b9",
+    "url": "/recipe/static/js/main.0aef75bc.chunk.js"
+  },
+  {
+    "revision": "70b0b55a4cc8a4f64c71",
+    "url": "/recipe/static/js/runtime-main.b2c80479.js"
+  }
+]);
